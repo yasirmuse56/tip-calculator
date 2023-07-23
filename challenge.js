35b029eadccd810121f1046ea64f5409ac27feaa
h. mikail <>
2023-07-23 05:28:00 -0700

    tip calculator exr sol

--- challenge.js
+++ challenge.js
@@ -1,2 +1,10 @@
-// Good Luck! You got this 💪🏾
-// Write your code here.
+const bill = 102; 
+let tipPercentage;
+if (bill >= 50 && bill <= 300) {
+  tipPercentage = 0.15;
+} else {
+  tipPercentage = 0.20;
+}
+const tip = bill * tipPercentage;
+const totalValue = bill + tip;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${totalValue}.`);
